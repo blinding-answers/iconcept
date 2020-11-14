8663f00879c8cf0b79296cb4c15932d9f6b57023
Jaco Labuschagne <zodimo@gmail.com>
2020-11-14 17:34:04 +0200

    test device_unit.py

---
+++ tests/test_device_unit.py
@@ -0,0 +1,23 @@
+from iconcept.messages.device_unit import DeviceUnit
+
+
+def test_is_valid_with_valid_datagram_is_true():
+    datagram = "550c0100"
+    device_unit = DeviceUnit(datagram)
+    assert True == device_unit.is_valid()
+
+
+def test_is_valid_with_invalid_datagram_is_false():
+    datagram = "invalid"
+    device_unit = DeviceUnit(datagram)
+    assert False == device_unit.is_valid()
+
+
+def test_get_unit_gives_valid_values_with_valid_datagram():
+    datagram = "550c0100"
+    device_unit = DeviceUnit(datagram)
+    assert 0 == device_unit.get_unit()
+
+    datagram = "550c0101"
+    device_unit = DeviceUnit(datagram)
+    assert 1 == device_unit.get_unit()
